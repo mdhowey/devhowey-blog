20edd028dc627f0d7871ddcc882e20aac8bca6f9
devhowey <dev.howey@gmail.com>
2021-11-17 15:47:47 -0600

    updating everything

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,6 @@
-import Header from '../../components/Header'
-import Posts from '../../components/Posts'
-import Sidebar from '../../components/Sidebar'
+import Header from '../../components/Header/Header'
+import Posts from '../../components/Posts/Posts'
+import Sidebar from '../../components/Sidebar/Sidebar'
 import classes from './Home.module.scss'
 
 export default function Home() {
