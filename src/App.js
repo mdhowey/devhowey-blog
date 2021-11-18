af733f2d288dca2ac2f1959557c06301b6651c69
devhowey <dev.howey@gmail.com>
2021-11-17 19:49:41 -0600

    worked on blog show page, made new form, working on profile settings page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
+// import Footer from './components/Footer/Footer';
 import Navbar from './components/Navbar/Navbar'
 // import Home from "./pages/home/Home";
-import ShowPost from './pages/showpost/ShowPost';
+// import ShowPost from './pages/showpost/ShowPost';
+import Newblog from './pages/newblog/Newblog';
 
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <ShowPost />
+      <Newblog />
+      {/* <Footer /> */}
     </div>
   );
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+import classes from './Footer.module.scss'
+import {BsGithub} from 'react-icons/bs'
+import {AiOutlineLinkedin} from 'react-icons/ai'
+
+export default function Footer() {
+  return (
+    <div className={classes.footer}>
+      <div className={classes.footer__center}>
+        <div className={classes.footer__center__links}>
+          <BsGithub className={classes.footer__center__links__icon}/>
+          <AiOutlineLinkedin className={classes.footer__center__links__icon}/>
+        </div>
+        <p className={classes.footer__center__howey}>&copy; devhowey 2021</p>
+      </div>
+    </div>
+  )
+}
