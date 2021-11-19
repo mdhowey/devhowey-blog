fba8659d163eb033338b567295da4b73ac2cf722
devhowey <dev.howey@gmail.com>
2021-11-19 09:04:22 -0600

    set up context files

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -7,23 +7,32 @@ export default function Register() {
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError] = useState(false);
 
   const handleSubmit = async (e) => {
     // prevents refreshing the page on submit
     e.preventDefault();
+    setError(false);
     try{
       const res = await axios.post(`/auth/register`, {
         username,
         email,
         password,
       });
+      // no error and data --> redirect to login
+      res.data && window.location.replace('/login');
     } catch (err) {
-      console.log(err)
+      setError(true);
     }
   };
   return (
     <div className={classes.register}>
       <h1 className={classes.register__title}>Register</h1>
+      {
+        error && <span className={classes.register__error}>
+          Username or email already in use
+        </span>
+      }
       <form 
         className={classes.register__form}
         onSubmit={handleSubmit}>
